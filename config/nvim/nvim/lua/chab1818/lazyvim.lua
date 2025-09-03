c8226374ab084040a4f90b8598bfecce1f8d975e
javierportillo <japqnx@gmail.com>
2025-09-03 10:29:00 -0400

    choose a different style

--- config/nvim/nvim/lua/chab1818/lazyvim.lua
+++ config/nvim/nvim/lua/chab1818/lazyvim.lua
@@ -89,6 +89,7 @@ return require('lazy').setup({
         -- Optionally configure and load the colorscheme
         -- directly inside the plugin declaration.
     vim.g.sonokai_enable_italic = true
+    vim.g.sonikai_style = 'andromeda'
     vim.cmd.colorscheme('sonokai')
     end
   }

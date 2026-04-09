6b0bb6e7822c085c172d8ce4cdc27797289b0e5d
Javier Portillo <japqnx@gmail.com>
2026-04-09 09:20:15 -0400

    add rax-x/go plugin

--- config/nvim/nvim/lua/chab1818/lazyvim.lua
+++ config/nvim/nvim/lua/chab1818/lazyvim.lua
@@ -92,5 +92,20 @@ return require('lazy').setup({
     vim.g.sonikai_style = 'andromeda'
     vim.cmd.colorscheme('sonokai')
     end
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    "tpope/vim-fugitive",
   }
 })

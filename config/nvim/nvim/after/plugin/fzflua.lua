5a7b4c2c2f536aef1c6d17712530987bdf181832
Javier Portillo <japqnx@gmail.com>
2026-04-09 09:09:19 -0400

    add fzf diagnostic and references

--- config/nvim/nvim/after/plugin/fzflua.lua
+++ config/nvim/nvim/after/plugin/fzflua.lua
@@ -13,3 +13,5 @@ vim.keymap.set("n", "<leader>fb", require("fzf-lua").buffers, {desc = "FzF Buffe
 vim.keymap.set("n", "<leader>fl", require("fzf-lua").blines, {desc = "FzF Lines"})
 vim.keymap.set("n", "<leader>fg", require("fzf-lua").grep, {desc = "FzF Grep"})
 vim.keymap.set("n", "<leader>fs", require("fzf-lua").treesitter, {desc = "FzF Symbols"})
+vim.keymap.set("n", "<leader>fd", require("fzf-lua").diagnostics_workspace, {desc = "FzF Workspace Diagnostic"})
+vim.keymap.set("n", "<leader>fr", require("fzf-lua").lsp_references, {desc = "FzF LSP References"})

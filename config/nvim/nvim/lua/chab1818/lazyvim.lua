def23f08416480ac07e793fcd978922eb13c717e
Javier Portillo <japqnx@gmail.com>
2026-05-04 16:05:50 -0400

    trying out a new languague

--- config/nvim/nvim/lua/chab1818/lazyvim.lua
+++ config/nvim/nvim/lua/chab1818/lazyvim.lua
@@ -146,4 +146,13 @@ return require('lazy').setup({
   "MeanderingProgrammer/render-markdown.nvim",
   ft = { "markdown", "codecompanion" }
   },
+  {
+   "ivov/lisette",
+   event = "VeryLazy",
+   config = function(plugin)
+     vim.opt.rtp:prepend(plugin.dir .. "/editors/nvim")
+     dofile(plugin.dir .. "/editors/nvim/ftdetect/lisette.lua")
+     dofile(plugin.dir .. "/editors/nvim/plugin/lisette.lua")
+   end,
+   },
 })

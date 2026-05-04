1bf39c2c7844da873438ce225f87b930b0538dbe
Javier Portillo <japqnx@gmail.com>
2026-05-04 15:59:02 -0400

    enabling

--- config/nvim/nvim/after/plugin/ale.lua
+++ config/nvim/nvim/after/plugin/ale.lua
@@ -1,5 +1,4 @@
-vim.cmd('let g:ale_linters = {"python": ["ruff"]}')
-vim.g.ale_enabled = 0
+vim.g.ale_enabled = 1
 vim.g.ale_disable_lsp = 0
 -- vim.keymap.set("n", "<leader>an", vim.cmd.ALENext)
 -- vim.keymap.set("n", "<leader>ap", vim.cmd.ALEPrevious)
